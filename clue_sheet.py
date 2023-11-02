2bb680418ee953be787961d9d9adc31a372432f6
RealTheBeastBoss <jack.cumbers@outlook.com>
2023-11-02 09:34:46 +0000

    Day Four: - More Work on the Clue Sheet Positioning

--- clue_sheet.py
+++ clue_sheet.py
@@ -10,16 +10,22 @@ def draw_text(text, font, colour, location):  # Draws text for the Clue Titles
     text_width = text_surface.get_width()
     text_height = text_surface.get_height()
     WINDOW.blit(text_surface, (location[0] - text_width, location[1] - (text_height/2)))
+    text_rect = text_surface.get_rect()
+    text_rect.topleft = (location[0] - text_width, location[1] - (text_height/2))
+    pygame.draw.rect(WINDOW, BLACK, text_rect, 1)
 
 class ClueSheet:
     def __init__(self):
-        self.clueTitles = [ClueTitle(MISS_SCARLETT, 80)]
+        self.clueTitles = [ClueTitle(MISS_SCARLETT, 245), ClueTitle(COL_MUSTARD, 280), ClueTitle(DR_ORCHID, 315), ClueTitle(REV_GREEN, 350), ClueTitle(MRS_PEACOCK, 385),
+                           ClueTitle(PROF_PLUM, 420), ClueTitle(CANDLESTICK, 475), ClueTitle(DAGGER, 510), ClueTitle(LEAD_PIPE, 545), ClueTitle(REVOLVER, 580), ClueTitle(ROPE, 615),
+                           ClueTitle(WRENCH, 650), ClueTitle(BALLROOM_CARD, 705), ClueTitle(BILLIARD_ROOM_CARD, 740), ClueTitle(CONSERVATORY_CARD, 775), ClueTitle(DINING_ROOM_CARD, 810),
+                           ClueTitle(HALL_CARD, 845), ClueTitle(KITCHEN_CARD, 880), ClueTitle(LIBRARY_CARD, 915), ClueTitle(LOUNGE_CARD, 950), ClueTitle(STUDY_CARD, 985)]
         self.selectedBox = None
 
     def draw(self):
-        pygame.draw.rect(WINDOW, WHITE, pygame.Rect((1300, 20), (1880, 700)), 0, 15)
+        pygame.draw.rect(WINDOW, WHITE, pygame.Rect((1326, 180), (564, 870)), 0, 15)
         for title in self.clueTitles:
-            draw_text(title.text, SMALL_FONT, BLACK, (1600, title.height))
+            draw_text(title.text, SMALL_FONT, BLACK, (1560, title.height))
 
 class ClueTitle:
     def __init__(self, card_reference, height):
